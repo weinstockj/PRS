62f0503a1ed09049f4f2aed07e9d5b0eb985aff1
Josh Weinstock <jweins17@jhu.edu>
2023-12-25 17:07:20 -0500

    adding precompilation with snoopprecompile

--- src/PRSFNN.jl
+++ src/PRSFNN.jl
@@ -12,11 +12,28 @@ using Random
 using StatsBase: sample
 using HypothesisTests: ApproximateTwoSampleKSTest
 using SnpArrays
+using LoggingExtras
+using Dates
+using SnoopPrecompile  
+
+# include in logging call, e.g., @info "$(ltime()) message"
+function ltime()
+    date_format = "yyyy-mm-dd HH:MM:SS"
+    return Dates.format(now(), date_format)
+end
 
 include("loss.jl")
 include("train.jl")
 include("simulate.jl")
 
 export joint_lob_prob, RSS, elbo, train_until_convergence
+
+# speed up precompilation for end users
+@precompile_setup begin
+    @precompile_all_calls begin
+        raw = simulate_raw()
+        ss = estimate_sufficient_statistics(raw[1], raw[3])
+    end
+end
 
 end
