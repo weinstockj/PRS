83b72a94f2e0cc1963773ba440e6fabdab4f4b77
akim126 <akim126@sr05.cm.cluster>
2024-01-18 21:37:01 -0500

    regularize sigma matrix

--- src/loss.jl
+++ src/loss.jl
@@ -49,8 +49,15 @@ function rss(β::Vector, coef::Vector, SE::Vector, R::AbstractArray)
     μ =  (SE .* R .* (1 ./ SE)') * β
     Σ = Hermitian(SE .* R .* SE')
     #println(Σ)
+    # there are some very small negative eigenvalues
+    # very close to zero in the covariance matrix Σ
+    # adding a small positive value to the diagonal of Σ
+    # for regularization to make the matrix positive definite
+    # Add λ to the diagonal of Σ
+    Σ_reg = Σ + λ * I
     # dist = MvNormal(μ, Σ)
-    return logpdf(MvNormal(μ, Σ), coef)
+    return logpdf(MvNormal(μ, Σ_reg), coef)
+    #return logpdf(MvNormal(μ, Σ), coef)
     #return μ, Σ
 end
 
