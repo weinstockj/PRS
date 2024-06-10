79167d4362da07bfa0750e89d695290bc2b6c652
Josh Weinstock <jweins17@jhu.edu>
2024-06-10 18:36:38 -0400

    modify unit test

--- test/nn_test.jl
+++ test/nn_test.jl
@@ -1,29 +1,39 @@
 function test_long_nn()
-    P = 100
+    P = 500
     K = 20
 
     model = Chain(
-            Dense(K => 5, relu; init = Flux.glorot_normal(gain = 0.0005)),
-            Dense(5 => 2)
+            Dense(K => 3, relu; init = Flux.glorot_normal(gain = 0.0005)),
+            Dense(3 => 2)
     )
 
     G = rand(Normal(0, 1), P, K)
     β = rand(Normal(0, 0.3), K)
 
-    q_var = 0.1 .* exp.(G * β)
+    q_var = 0.05 .* exp.(G * β)
     q_α = 1.0 ./ (1.0 .+ exp.(-1.0 .* (-2.0 .+ q_var)))
+
+    PRSFNN.describe_vector(q_var)
+    PRSFNN.describe_vector(q_α)
 
     trained_model = PRSFNN.fit_heritability_nn(
             model, 
             q_var, 
             q_α, 
             G;
-            patience = 100
+            max_epochs = 4000,
+            patience = 100,
+            mse_improvement_threshold = 0.01,
+            # optim_type = Momentum(0.01, 0.9)
+            optim_type = AdamW(0.02)
     )
 
     yhat = transpose(trained_model(transpose(G)))
     yhat[:, 1] .= exp.(yhat[:, 1])
     yhat[:, 2] .= 1.0 ./ (1.0 .+ exp.(-yhat[:, 2]))
+
+    PRSFNN.describe_vector(yhat[:, 1])
+    PRSFNN.describe_vector(yhat[:, 2])
 
     @test cor(yhat[:, 1], q_var) > .7
     @test cor(yhat[:, 2], q_α) > .7
